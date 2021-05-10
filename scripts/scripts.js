bc13c4f47bc7c55e886d31ef280f910899318d07
0uz <duymaz_14@hotmail.com>
2021-05-11 01:25:30 +0300

    last

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,12 @@
-  function openForm() {
+function openForm() {
     document.getElementById("loginForm").style.display = "block";
   }
   
-  function closeForm() {
-    document.getElementById("loginForm").style.display = "none";
+  function closeForm(id) {
+    document.getElementById(id).style.display = "none";
   }
 
   function openRegForm() {
-    closeForm()
+    closeForm("loginForm")
     document.getElementById("regForm").style.display = "block";
   }
