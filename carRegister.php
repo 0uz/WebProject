60517ec9784689837cd822f92ad4b4c7099e2639
0uz <duymaz_14@hotmail.com>
2021-05-15 16:20:35 +0300

    last

--- carRegister.php
+++ carRegister.php
@@ -1,14 +1,20 @@
 <?php 
     $db = mysqli_connect("localhost","root","","car_rent");
+    $carBrand = mysqli_real_escape_string($db,$_POST['carBrand']);
     $carName = mysqli_real_escape_string($db,$_POST['carName']);
     $carPrice = mysqli_real_escape_string($db,$_POST['carPrice']);
     $deposit = mysqli_real_escape_string($db,$_POST['deposit']);
     $gear = mysqli_real_escape_string($db,$_POST['gear']);
     $fuel = mysqli_real_escape_string($db,$_POST['fuel']);
-    $imageFile = mysqli_real_escape_string($db,$_POST['imageFile']);
-    
-    /* TODO
-    $query =
+    $imageFile = mysqli_real_escape_string($db,$_FILES["imageFile"]["name"]);
+    $cityID = mysqli_real_escape_string($db,$_POST['city']);
+    $query ="INSERT INTO cars (carBrand,carName,carPrice,deposit,gear,fuel,photoPath,cityID) VALUES ('$carBrand','$carName',$carPrice,$deposit,'$gear','$fuel','$imageFile',$cityID)";
     $result = mysqli_query($db,$query);
-    header("Location: http://localhost/WebProject/index.php")*/
+
+    //--------------------UPLOAD IMAGE-----------------------
+    $target_dir = "images/"; // folder path
+    $target_file = $target_dir . basename($_FILES["imageFile"]["name"]);
+    move_uploaded_file($_FILES["imageFile"]["tmp_name"], $target_file);
+
+    header("Location: http://localhost/WebProject/index.php");
 ?>
--- login.php
+++ login.php
@@ -4,14 +4,18 @@
         $db = mysqli_connect("localhost","root","","car_rent");
         $mail = $_POST['email'];
         $pass = $_POST['psw'];
-        $query = "SELECT id,F_name,L_name FROM user WHERE email='$mail' AND password = '$pass'";
+        $query = "SELECT id,F_name,L_name,isAdmin FROM user WHERE email='$mail' AND password = '$pass'";
         $result = mysqli_query($db,$query);
         if(mysqli_num_rows($result) == 1){
             session_start();
             $row = mysqli_fetch_array($result);
             $_SESSION['user'] = $row['id'];
             $_SESSION['name'] = $row['F_name']." ".$row['L_name'];
-            header("Location: http://localhost/WebProject/index.php");
+            if($row['isAdmin'] == 1){
+                header("Location: http://localhost/WebProject/admin.php");
+            }else{
+                header("Location: http://localhost/WebProject/index.php");
+            }
         }else{
             header("Location: http://localhost/WebProject/index.php");
         }
